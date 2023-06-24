86fb4c9c40e3bd640e198edfdfa93bdbaa71a120
Murilo Finardi <101904284+MuriloFin@users.noreply.github.com>
2023-06-24 15:28:27 -0300

    Global Solution - Fome

---
+++ Global Solution/js/script.js
@@ -0,0 +1,105 @@
+function hideandshow() {
+    var x = document.getElementById("dadosfome");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideandshow2() {
+    var x = document.getElementById("dadosfome2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideandshow3() {
+    var x = document.getElementById("dadosfome3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideandshow4() {
+    var x = document.getElementById("dadosfome4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideandshow5() {
+    var x = document.getElementById("dadosfome5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideandshow6() {
+    var x = document.getElementById("dadosfome6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function contribua() {
+  alert('Contribua para que a fome seja combatida! Pequenas atitudes salvam vidas. Agora podemos continuar!');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var page2 = document.querySelector(".page2");
+
+  page2.addEventListener("mouseover", function() {
+    page2.style.backgroundColor = "#A4303F";
+  });
+
+  page2.addEventListener("mouseout", function() {
+    page2.style.backgroundColor = "#362C28";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var page3 = document.querySelector(".page3");
+
+  page3.addEventListener("mouseover", function() {
+    page3.style.backgroundColor = "#A4303F";
+  });
+
+  page3.addEventListener("mouseout", function() {
+    page3.style.backgroundColor = "#362C28";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var page4 = document.querySelector(".page4");
+
+  page4.addEventListener("mouseover", function() {
+    page4.style.backgroundColor = "#A4303F";
+  });
+
+  page4.addEventListener("mouseout", function() {
+    page4.style.backgroundColor = "#362C28";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var projeto = document.querySelector(".projeto");
+
+  projeto.addEventListener("mouseover", function() {
+    projeto.style.backgroundColor = "#A4303F";
+  });
+
+  projeto.addEventListener("mouseout", function() {
+    projeto.style.backgroundColor = "#362C28";
+  });
+});
